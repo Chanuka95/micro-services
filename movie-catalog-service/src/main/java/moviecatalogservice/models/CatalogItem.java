47d60efdc89a32aec8b2d958bcdedb0ec1b82176
Chanuka.nuwankalpa <Chanuka.nuwankalpa@iihsolutions.com>
2023-03-16 06:21:01 +0530

    example to micro service

---
+++ movie-catalog-service/src/main/java/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,17 @@
+package moviecatalogservice.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CatalogItem {
+
+    private String name;
+    private String description;
+    private int rating;
+
+
+
+}
